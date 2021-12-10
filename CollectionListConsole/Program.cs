1265d643c64187c6e02f106ff4f5fb5073acb12d
dev-Omkar <opawar88@gmail.com>
2021-12-10 14:15:59 +0530

    Working Model

--- CollectionListConsole/Program.cs
+++ CollectionListConsole/Program.cs
@@ -6,12 +6,63 @@ namespace CollectionListConsole
 {
     class Program
     {
+        static List<Student> Division1 = new List<Student>();
+        static List<Student> Division2 = new List<Student>();
+
         public static void Main()
         {
-            Student.Add();
-            Student.View();
-            Student.Delete();
-        } 
+            Program.Menu(); // Display Menu
+        }
+        public static void Chose(int n)
+        { 
+            switch (n)
+            {
+                case 1:
+                    Student.Add();
+                    break;
+                case 2:
+
+                    Student.View();
+                    break;
+                case 3:
+                    Student.FindOrDelete();
+                    break;
+                case 4:
+                    Student.Total();
+                    break;
+                case 5:
+                    Environment.Exit(0); // For exit
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry!");
+                    break;
+            }
+        }
+        public static void Menu()
+        {
+            bool exitMenuLoop = false;           
+            do
+            {
+                Console.WriteLine("===================================================================");
+                Console.WriteLine(" 1 - Add Student ");
+                Console.WriteLine(" 2 - View Student ");
+                Console.WriteLine(" 3 - Find or Delete Student ");
+                Console.WriteLine(" 4 - Exit");
+                Console.WriteLine("Enter Your option");
+                
+                int option = int.Parse(Console.ReadLine());
+                
+                if (option > 0 && option < 5)
+                {
+                    Program.Chose(option);
+                }
+                else
+                {
+                    Console.WriteLine("\nThe selection has to be between 1 and 4, please re-enter your selection");
+                }
+
+            } while (!exitMenuLoop);
+        }      
 
         public class Student 
         {
@@ -19,22 +70,29 @@ namespace CollectionListConsole
             public int RollNo { get; set; }
             public string SubName { get; set; }
 
-            static List<Student> Division1 = new List<Student>();
-            static List<Student> Division2 = new List<Student>();
-
+            //Add Student
             public static void Add()
             {
-                
+
                 Console.WriteLine("Enter Total Student : ");
                 int total = int.Parse(Console.ReadLine());
-                //Add Student
                 do
                 {
+                    //For Auto Genrated Roll No
+                    int rn;
+                    if(Total()==0)
+                    {
+                        rn =1;
+                    }
+                    else
+                    {
+                        rn = Total() + 1;
+                    }
+
                     Console.WriteLine("-----------------------------------------------------------------------------");
-
-                    Console.Write("Enter Student Roll No  : ");
-                    int no = int.Parse(Console.ReadLine());
-
+                    
+                    Console.Write("Student Roll No  : {0} \n",rn);
+                  
                     Console.Write("Enter Student Name     : ");
                     string name = Console.ReadLine();
 
@@ -44,7 +102,7 @@ namespace CollectionListConsole
                     Student student = new Student()
                     {
                         Name = name,
-                        RollNo = no,
+                        RollNo = rn,
                         SubName = subName
                     };
 
@@ -59,11 +117,13 @@ namespace CollectionListConsole
                         total--;
                     }
 
-                } while (total > 0);                
+                } while (total > 0);
             }
+
+            //All Student List
             public static void View()
             {
-                Console.WriteLine("========================================== All Student ===========================================");
+                Console.WriteLine("========================================== All Student =====================================================");
                
                 // All Student
                 var result = Division1.Concat(Division2).OrderBy(x => x.RollNo).ToList();
@@ -82,40 +142,60 @@ namespace CollectionListConsole
                 }
 
                 Console.WriteLine();
-
+               
                 //Display Division 2 List
                 foreach (var division2 in Division2)
                 {
-                    Console.WriteLine("Student Division 2 => \t Roll No : {0} \t Name  : {1} \tSubject Name : {2}", division2.RollNo, division2.Name, division2.SubName);
+                    Console.WriteLine("Student Division 2 => \tRoll No : {0} \t Name  : {1} \tSubject Name : {2}", division2.RollNo, division2.Name, division2.SubName);
                 }
 
             }
-            
-            public static void FindStudent(int rollno)
+
+            //Find
+            public static void FindStudent(int division, int rollno)
             {
-                Division2.ForEach(item => Division1.Add(item));
-               var result= Division2.Single(s => s.RollNo == rollno);
-                Console.WriteLine("{0} {1} {2}",result.RollNo,result.Name,result.SubName);
-                //  string result = Division1.Single(s => s == search);
+
+                if (division == 1)
+                {
+                    var result = Division1.Single(s => s.RollNo == rollno);
+                    Console.WriteLine("Division {0} => \tRoll No : {1} \t Name  : {2} \tSubject Name : {3}", division, result.RollNo, result.Name, result.SubName);
+                }
+                if (division == 2)
+                {
+                    var result = Division2.Single(s => s.RollNo == rollno);
+                    Console.WriteLine("Division {0} => \tRoll No : {1} \t Name  : {2} \tSubject Name : {3}", division, result.RollNo, result.Name, result.SubName);
+                }
             }
 
-            public static void Delete()
+            public static int Total()
+            {
+                var result = Division1.Concat(Division2).OrderBy(x => x.RollNo).ToList();
+                return result.Count();
+            }
+
+            //Find And Delete
+            public static void FindOrDelete()
             {
                 Console.WriteLine("========================================== Delete Student ===========================================");
+                Console.Write("Enter Division (1 or 2): ");
+                int division = int.Parse(Console.ReadLine());
+                
                 Console.Write("Enter Roll No : ");
-
                 int rollno = int.Parse(Console.ReadLine());
-                FindStudent(rollno);
-                Console.WriteLine("Do you want to delete this Student ? ( Y / N )");
-                if ("Y" == Console.ReadLine().ToUpper())
+                
+                FindStudent(division, rollno);
+                Console.WriteLine("Do you want to delete or edit this Student( D = delete | N = Exit) ? ");
+                
+                if ("D" == Console.ReadLine().ToUpper())
                 {
                     Division1.RemoveAll(x => x.RollNo == rollno);
                     Division2.RemoveAll(x => x.RollNo == rollno);
                 }
                 else
                 {
-                    Console.WriteLine("You cancel this Process !!! ");
+                    Console.WriteLine("You cancel Process !!! ");
                 }
+
                 View();
             }
         }
